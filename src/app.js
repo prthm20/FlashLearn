215a0c67b9623f561f8a7555bc8450445b59bc6d
prthm20 <aprathamm@gmail.com>
2025-04-04 21:02:54 +0530

    router

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express, { urlencoded } from "express";
 import cookieParser from "cookie-parser";
+import cors from 'cors'
 const app = express();
 
 app.use(cors({
@@ -14,4 +15,12 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
+//routes import 
+
+import userrouter from './routes/user.routes.js'
+
+//routes declaration
+app.use("/api/v1/users",userrouter)
+
+"http://localhost:8000/api/v1/user/route"
 export default app; 
