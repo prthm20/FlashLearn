6152ca8a5854580287bfdd67856751e0665ab093
prthm20 <aprathamm@gmail.com>
2025-04-10 04:12:31 +0530

    frontend

--- src/controllers/Flashcard.controller.js
+++ src/controllers/Flashcard.controller.js
@@ -2,11 +2,13 @@ import axios from "axios";
 import { Note } from "../models/Notes.model.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import OpenAI from "openai";
+import { ApiResponse } from "../utils/ApiResponse.js";
 const openai = new OpenAI({ apiKey: process.env.OPEN_AI_KEY});
 
 const flashcards=asyncHandler( async (req,res) => {
-    const { noteId } = req.body;
-    const note = await Note.findById(noteId);
+    const { summary } = req.query;
+    const note = summary;
+    console.log(summary)
   
     const completion = await openai.chat.completions.create({
         model: "gpt-4o", 
@@ -14,16 +16,19 @@ const flashcards=asyncHandler( async (req,res) => {
             { role: "system", content: "You are a Notes maker create flash cards" },
             {
                 role: "user",
-                content:`Create flashcards from this text: ${note.summary}`,
+                content:`Create flashcards from this text: ${summary}`,
             },
         ],
     });
      
     console.log(completion.choices)
-    const flash = completion.choices.message
-    note.flashcards=flash
-    await note.save();
-    return completion.choices[0].message.content;
+    const flash = completion.choices
+    console.log(flash)
+   // note.flashcards=flash
+   // await note.save();
+   return res.status(201).json(
+    new ApiResponse(200,"notes created succesfully",flash)
+) ;
 }
 )
 
--- src/controllers/Notes.controller.js
+++ src/controllers/Notes.controller.js
@@ -3,10 +3,12 @@ import { Note } from "../models/Notes.model.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import OpenAI from "openai";
 import { ApiResponse } from "../utils/ApiResponse.js";
-const openai = new OpenAI({ apiKey: process.env.OPEN_AI_KEY});
+const openai = new OpenAI({ apiKey:process.env.OPEN_AI_KEY});
 
 const Notes=asyncHandler( async (req,res) => {
-  const {text,userId} = req.body
+  const {text} = req.query
+  console.log(req.query)
+  console.log(text)
     const completion = await openai.chat.completions.create({
         model: "gpt-4o", 
         messages: [
@@ -18,10 +20,10 @@ const Notes=asyncHandler( async (req,res) => {
         ],
     });
      
-    console.log(completion.choices[0].message.content)
+    console.log("Result:",completion.choices[0].message.content)
     const summary = completion.choices[0].message.content;
-    const note = new Note({ userId, originalText: text, summary });
-    await note.save();
+    //const note = new Note({ userId, originalText: text, summary });
+    //await note.save();
      return res.status(201).json(
         new ApiResponse(200,"notes created succesfully",summary)
     ) ;
